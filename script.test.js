41c26fde833955710c5e78e7bc1aa20dcf30e563
Natalia Romankevich <romankevich.law@gmail.com>
2021-01-13 22:14:00 +0100

    grouped GoogleSearch tests, finished googleSearch tests

--- script.test.js
+++ script.test.js
@@ -1,3 +1,28 @@
-it('this is a test', ()=> {
+const googleSearch = require('./script');
+
+dbMock = [
+     'dog.com',
+     'cheesepuff.com',
+     'dicney.com',
+     'dogpictures.com',
+];
+
+describe('googleSearch', () =>{
+    it('it is a silly test', ()=> {
+        expect('hello').toBe('hello')
+    });
     
-})
+    it('is searching google', ()=> {
+        expect(googleSearch('testtest',dbMock)).toEqual([])
+        expect(googleSearch('dog',dbMock)).toEqual(['dog.com','dogpictures.com'])
+    });
+    
+    it('work with undefined and null input', ()=> {
+        expect(googleSearch(undefined,dbMock)).toEqual([]);
+        expect(googleSearch(null,dbMock)).toEqual([]);
+    });
+    
+    it('does not return more than 3 matches', ()=> {
+        expect(googleSearch('.com',dbMock).length).toEqual(3);
+    });
+});
