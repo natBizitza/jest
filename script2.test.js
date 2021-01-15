bc923dee47b24b5479e5392fafa8cdbe8fdf0a1d
Natalia Romankevich <romankevich.law@gmail.com>
2021-01-15 23:04:13 +0100

    finished async testing, used assertions and doen method

--- script2.test.js
+++ script2.test.js
@@ -1,2 +1,21 @@
 const fetch = require('node-fetch');
 const swapi = require('./script2');
+
+it('calls swapi to get people', (/* done */)=> {
+    /* always use assertions to test async code */
+    expect.assertions(1)
+    /* return OR done to wait for async */
+    return swapi.getPeople(fetch).then(data => {
+        expect(data.count).toEqual(82)
+       /*  done(); */
+    })
+})
+
+it('calls swapi to get people with a promise', ()=> {
+    expect.assertions(2)
+
+    return swapi.getPeoplePromise(fetch).then(data => {
+        expect(data.count).toEqual(82)
+        expect(data.results.length).toBeGreaterThan(5);
+    })
+})
